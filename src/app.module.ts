68e075da85f1e46021ce416340e500c396e97799
Kodeneko <ladysun.freedom@gmail.com>
2025-10-16 12:40:45 +0200

    [dev] env files + init config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsModule } from './posts/posts.module';
+import { ActivityModule } from './activity/activity.module';
+import { TrendingModule } from './trending/trending.module';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+const DB_URL = process.env.DB_URL as string;
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(DB_URL),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PostsModule,
+    ActivityModule,
+    TrendingModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
